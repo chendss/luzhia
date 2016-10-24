3fda64f36f56bfbe6f94169341b88e5bbdad8719
chendss <capslock.ch@outlook.com>
2016-10-25 01:37:25 +0800

    屏幕录制程序源码
    桌面屏幕录制调用ffmpeg,DirectX捕获音频设备。
    
    ffmpeg太大我不上传了。去https://ffmpeg.org/下WINDOWS版的，解压后放在\bin\ffmpeg-20161012-7cf0ed3-win64-static\ffmpeg-20161012-7cf0ed3-win64-static中

---
+++ zhibo/zhibo/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.DirectX;
+using Microsoft.DirectX.DirectSound;
+using System.IO;
+namespace zhibo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ffemg ffs = new ffemg();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 开始按钮事件：按下则灰色，执行录制函数。若列表是空则跳出
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            string recodequipmentText = RecodEquipment.Text.ToString();
+            if (recodequipmentText == "")
+            {
+                MessageBox.Show("请选择一个设备！");
+                return;
+            }
+            bool err;
+            ffs.Start(recodequipmentText,@"D:\wss.m3u8",out err);
+            if (err)
+            {
+                EndButton.IsEnabled = true;
+                StartButton.IsEnabled = false;
+            }
+            
+
+        }
+        /// <summary>
+        /// 结束按钮事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EndButton_Click(object sender, RoutedEventArgs e)
+        {
+            ffs.Stop();
+            EndButton.IsEnabled = false;
+            StartButton.IsEnabled = true;
+
+        }
+
+
+        /// <summary>
+        /// 程序一载入执行事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RecodEquipment_Initialized(object sender, EventArgs e)
+        {
+            /// <summary>
+            /// 
+            /// 获得音频设备名，并且赋值给下拉框
+            /// </summary>
+            /// <param name="sender"></param>
+            /// <param name="e"></param>
+            CaptureDevicesCollection captureCollection = new CaptureDevicesCollection();//获取录音设备
+            string str = "";
+            for (int i = 0; i < captureCollection.Count; i++)//获取每个设备名的值
+            {
+                if (i != 0)
+                {
+                    str = captureCollection[i].Description;
+                    RecodEquipment.Items.Add(str);
+                }
+            }
+        }
+    }
+}
---
+++ zhibo/zhibo/ffmpeg.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Microsoft.DirectX.DirectSound;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+namespace zhibo
+{
+    class ffemg
+    {
+        #region 控制台api
+
+        [DllImport("kernel32.dll")]
+        static extern bool GenerateConsoleCtrlEvent(int dwCtrlEvent, int dwProcessGroupId);
+
+        [DllImport("kernel32.dll")]
+        static extern bool SetConsoleCtrlHandler(IntPtr handlerRoutine, bool add);
+
+        [DllImport("kernel32.dll")]
+        static extern bool AttachConsole(int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        static extern bool FreeConsole();
+
+        #endregion
+
+
+        // 访问ffmpeg进程
+        static Process p = new Process();
+
+        // ffmpeg.exe路径
+        static string ffmpegPath = AppDomain.CurrentDomain.BaseDirectory + @"\ffmpeg-20161012-7cf0ed3-win64-static\ffmpeg-20161012-7cf0ed3-win64-static\bin\ffmpeg.exe";
+        int a;
+        /// <summary>
+        /// 功能: 录制
+        /// </summary>
+        public void Start(string audio,string outFilePath,out bool err)//输出路径
+        {
+            err = true;
+            string ces;
+            if (File.Exists(outFilePath))
+            {
+                File.Delete(outFilePath);
+            }
+            ProcessStartInfo startInfo = new ProcessStartInfo(ffmpegPath);
+            startInfo.WindowStyle = ProcessWindowStyle.Normal;
+            startInfo.Arguments = "-f dshow -i audio=\"" + audio 
+                                + "\"" + " -draw_mouse 1 -offset_x 0 -offset_y 0 -f GDIgrab -i desktop -vcodec libx264 -hls_time 4 -hls_list_size 0 " 
+                                + outFilePath;//相当于在CMD输入的东西，ffmpeg参数
+            p.StartInfo = startInfo;
+            //隐藏cmd
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.CreateNoWindow = true;
+            //程序开始
+            try
+            {
+                p.Start();
+            }
+            catch (Exception e)
+            {
+                if (e.Message.Contains("找不到"))
+                {
+                    MessageBox.Show("屏幕录制库已丢失");
+                    err = false;
+                }
+                
+            }
+        }
+
+        /// <summary>
+        /// 功能: 停止录制 模拟Ctrl+c
+        /// </summary>
+        public void Stop()
+        {
+
+            AttachConsole(p.Id);
+            SetConsoleCtrlHandler(IntPtr.Zero, true);
+            GenerateConsoleCtrlEvent(0, 0);
+            FreeConsole();
+
+            
+        }
+
+    }
+}
